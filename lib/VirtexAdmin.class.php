fd27edf298a1e9bbaed32b63f0080e18eef7c41c
mosman <mosman>
2006-02-07 14:20:30 +0000

    Adicionado funcionalidade de banco de dados utilizando MDatabase, do framework Mosman

--- lib/VirtexAdmin.class.php
+++ lib/VirtexAdmin.class.php
@@ -1,13 +1,47 @@
 <?
 
 require_once("MWebApp.class.php");
+require_once("MDatabase.class.php");
 
 class VirtexAdmin extends MWebApp {
 
+	protected $bd;
 
 	public function VirtexAdmin() {
 	   parent::MWebApp("etc/virtex.ini",'template/default');
-	
+	   
+	   if( @$this->cfg->config["DB"]["dsn"] ) {
+	      // Instanciar BD;
+	      
+	      $this->bd = new MDatabase($this->cfg->config["DB"]["dsn"]);
+	      
+	      
+	      if( $this->bd->obtemErro() != MDATABASE_OK ) {
+	      
+	         echo "ERRO: " . $this->bd->obtemMensagemErro() . "<br>\n";
+	      
+	      }
+	      
+	      
+	      
+	      
+	      /**
+	      $options = array(
+	      	               "debug" => 0,
+	      	               "portability" => DB_PORTABILITY_ALL
+	      	              );
+	      
+	      $this->bd =& DB::connect($this->cfg->config["DB"]["dsn"],$options);
+	      
+	      if( PEAR::isError($this->bd) ) {
+	         echo "ERRO: " . $this->bd->getMessage();
+	      }
+	      
+	      */
+	      
+	      
+	      
+	   }
 	}
 	
 	
