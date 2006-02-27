f8250e09a63200e2023befd6ee906a7b03455bea
hugo <hugo>
2006-02-27 13:28:35 +0000

    *** empty log message ***

---
+++ lib/redes.class.php
@@ -0,0 +1,168 @@
+<?
+   /**
+    * Manipulagco de redes ip
+    * CopyRight(C) 2004 - Andrei de Oliveira Mosman
+    * Todos os Direitos Reservados
+    */
+
+   function d2b($num) {
+      return base_convert($num,10,2);
+   }
+
+   function b2d($num) {
+      return (double)base_convert($num,2,10);
+   }
+
+   function to8bin($numero) {
+      return( str_pad(d2b($numero),8,'0',STR_PAD_LEFT) );
+   }
+
+   function addr2bin($addr) {
+      list($a,$b,$c,$d) = explode(".",$addr);
+      return( to8bin($a).to8bin($b).to8bin($c).to8bin($d) );
+   }
+
+   function bin2addr($bin) {
+      $bin = str_pad($bin,32,'0',STR_PAD_LEFT);
+      $a = substr($bin,0,8);
+      $b = substr($bin,8,8);
+      $c = substr($bin,16,8);
+      $d = substr($bin,24,8);
+
+      return(b2d($a).".".b2d($b).".".b2d($c).".".b2d($d));
+   }
+
+   function bitcount2bitmask($bitCount) {
+      return( str_repeat('1',$bitCount) . str_repeat('0', 32 - $bitCount) );
+   }
+
+   class RedeIP {
+      var $addr;
+      var $mask;
+      var $network;
+      var $wildcard;
+      var $broadcast;
+
+      function RedeIP($rede,$mask="") {
+         $bits=0;
+         if( $mask ) {
+            $this->mask = addr2bin($mask);
+         } else {
+            list($rede,$bits) = explode("/", $rede);
+            $this->mask = bitcount2bitmask($bits);
+         }
+         $this->addr = addr2bin($rede);
+         $this->network = $this->addr & $this->mask;
+         $this->wildcard = str_pad( d2b( b2d($this->mask) ^ b2d(str_repeat('1',32))), 32, '0', STR_PAD_LEFT);
+
+         if( $bits ) {
+            $wc = str_repeat('0', $bits ) . str_repeat('1', 32 - $bits );
+            $this->wildcard = $wc;
+         } else {
+            $this->wildcard = str_pad( d2b( b2d($this->mask) ^ b2d(str_repeat('1',32))), 32, '0', STR_PAD_LEFT);
+         }
+         $this->broadcast = $this->network | $this->wildcard;
+      }
+
+      // Retorna lista com todos os ips validos de uma determinada classe.
+      function listaIPs() {
+         $retorno = Array();
+         for($x=b2d($this->network)+1;$x<b2d($this->broadcast);$x++) {
+            $retorno[] = bin2addr(d2b($x));
+            //echo bin2addr(d2b($x)) . "<br>\n";
+         }
+         return($retorno);
+      }
+
+      function numHosts() {
+         return( b2d($this->broadcast) - (b2d($this->network)+1) );
+      }
+
+      function minHost() {
+         return( bin2addr(d2b(b2d($this->network)+1)) );
+      }
+
+      function maxHost() {
+         return( bin2addr(d2b(b2d($this->broadcast)-1)) );
+      }
+      function numIPs() {
+         return( b2d($this->broadcast) - b2d($this->network) + 1 );
+      }
+
+      /**
+       * Pega o numero de bits de uma dada mascara e subclasseia a rede
+       * retorna um array de objetos RedeIP
+       */
+
+      function listaSubRedes($bits) {
+
+         $retorno = Array();
+
+         $fim = 0;
+
+         $rede = b2d($this->network);
+
+         while( $rede < b2d($this->broadcast) ) {
+            $subrede = new RedeIP( bin2addr(d2b($rede)) . "/" . $bits );
+
+            //echo "NW:".b2d($subrede->network) . "<br>NW:" . b2d($this->network) . "<br><br>\n";
+            //echo "BC:".b2d($subrede->broadcast) . "<br>BC:" . b2d($this->broadcast) . "<br><br>\n";
+
+            if( b2d($subrede->network) >= b2d($this->network) && b2d($subrede->broadcast) <= b2d($this->broadcast) ) {
+               // echo bin2addr($subrede->network) . "/" . $bits . "<br>\n";
+               $retorno[] = $subrede;
+            }
+
+            $rede += $subrede->numIPs();
+         }
+
+         return($retorno);
+      }
+
+      function imprimeDebug() {
+
+         echo "A: " . $this->addr ."(". bin2addr($this->addr)  .")". "<br>\n";
+         echo "M: " . $this->mask ."(". bin2addr($this->mask)  .")". "<br>\n";
+         echo "N: " . $this->network ."(". bin2addr($this->network)  .")". "<br>\n";
+         echo "W: " . $this->wildcard ."(". bin2addr($this->wildcard)  .")". "<br>\n";
+         echo "BC:" . $this->broadcast ."(". bin2addr($this->broadcast)  .")". "<br>\n";
+         echo "<hr>";
+         echo "IPs: " . $this->numIPs() . "<br>\n";
+         echo "numHosts: " . $this->numHosts() . "<br>\n";
+         echo "minHost: " . $this->minHost() . "<br>\n";
+         echo "maxHost: " . $this->maxHost() . "<br>\n";
+
+         echo "<hr>";
+         //$this->listaIPs();
+         //echo "<hr>";
+
+
+      }
+
+
+   }
+
+   //$teste = new RedeIP("192.168.2.128/25");
+
+   //echo "A: " . $teste->addr . "<br>\n";
+   //echo "M: " . $teste->mask . "<br>\n";
+   //echo "N: " . $teste->network . "<br>\n";
+   //echo "W: " . $teste->wildcard . "<br>\n";
+   //echo "BC:" . $teste->broadcast . "<br>\n";
+   //echo "<hr>";
+   //echo "IPs: " . $teste->numIPs() . "<br>\n";
+   //echo "numHosts: " . $teste->numHosts() . "<br>\n";
+   //echo "minHost: " . $teste->minHost() . "<br>\n";
+   //echo "maxHost: " . $teste->maxHost() . "<br>\n";
+
+   //echo "<hr>";
+   //$teste->listaIPs();
+   //echo "<hr>";
+   //$bitsSubRede = 30;
+   //$subRedes = $teste->listaSubRedes($bitsSubRede);
+
+   //for( $x=0;$x<count($subRedes);$x++ ) {
+   //   echo bin2addr($subRedes[$x]->network) . "/" . $bitsSubRede . "<br>\n";
+   //}
+
+?>
