b49ec39a6b4d2e9a602dfab342e4ae591703ca51
hugo <hugo>
2006-03-05 17:34:55 +0000

    no message

---
+++ doc/sequences.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE clsq_id_cliente;
+CREATE SEQUENCE cbsq_id_cliente_produto;
+CREATE SEQUENCE cbsq_id_cobranca;
+CREATE SEQUENCE prsq_id_produto;
+CREATE SEQUENCE adsq_id_admin;
+CREATE SEQUENCE adsq_id_priv;
+CREATE SEQUENCE cfsq_id_pop;
+CREATE SEQUENCE cfsq_id_nas;
