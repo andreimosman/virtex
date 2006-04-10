e42a8abc7f9ee7c5b5e6c4e30daac7e12f7a349a
dede <dede>
2006-04-10 14:35:53 +0000

    no message

--- lib/VAClientes.class.php
+++ lib/VAClientes.class.php
@@ -736,7 +736,7 @@ class VAClientes extends VirtexAdmin {
 						//	echo "SQL: $sSQL <br>\n";
 						//}
 						
-						
+												
 						if ($email_igual == "1"){
 							$sSQL  = "SELECT * from cftb_preferencias where id_provedor = '1'";
 							$prefs = $this->bd->obtemUnicoRegistro($sSQL);
@@ -1112,7 +1112,9 @@ class VAClientes extends VirtexAdmin {
 					$sSQL  = "SELECT ";
 					$sSQL .= "  id_cobranca, nome_cobranca, tipo_cobranca ";
 					$sSQL .= "FROM ";
-					$sSQL .= "cbtb_cobranca ";
+					$sSQL .= "cftb_forma_pagamento ";
+					$sSQL .= "WHERE ";
+					$sSQL .= "	disponivel = 't'";
 											
 					$tipo_cobranca = $this->bd->obtemRegistros($sSQL);
 					
@@ -1207,7 +1209,55 @@ class VAClientes extends VirtexAdmin {
 					}
 					
 					
+//DEDÉ
+					//Verifica o cadastramento anterior do contrato
 					
+					$id_cliente_produto = @$_REQUEST["id_cliente_produto"];
+					$sSQL = "SELECT COUNT(*) FROM cbtb_contrato WHERE id_cliente_produto = $id_cliente_produto";
+					
+					
+										
+					
+					//Cadastro de contrato					
+					//Pega informações sobre forma de pagamento					
+					$tipo_cobranca = @_$REQUEST['tipo_cobranca'];
+					
+					$sSQL = "SELECT * FROM cftb_forma_pagamento WHERE tipo_cobranca = $tipo_cobranca ";
+					
+					//Coleta informações sobre o tipo de pagamento escolhido
+					$forma_pagamento = $this->bd->obtemUnicoRegistro($sSQL);
+										
+					
+					$vigencia = @$_REQUEST["vigencia"];
+					$data_renovacao = @$_REQUEST["data_renovacao"];
+					$valor_contrato = @$_REQUEST["valor_contrato"];
+					$id_cobranca = $tipo_cobranca;
+					$status = @REQUEST["status"];
+					
+					$sSQL = "INSERT INTO cbtb_contrato (";
+					$sSQL .= "	id_cliente_produto, vigencia, data_renovacao, valor_contrato, id_cobranca, status";
+					$sSQL .= ") VALUES (";
+					$sSQL .= "	$id_cliente_produto, $vigencia, '$data_renovacao', $valor_contrato, $id_cobranca, '$status'";
+					
+					$this->bd->consulta($sSQL);
+										
+					/*
+					//Taxa de juros padrao
+					$sSQL = "SELECT * from cftb_preferencias WHERE id_provedor = 1";
+					$prov_prefs = $this->bd->obtemUnicoRegistro($sSQL);					
+					$tx_juros = prev_profs["tx_juros"];
+					
+					//Cadastro da Forma de Pagamento
+					
+					$valor = @$_RESQUEST["valor"];
+					
+					
+					$sSQL = "INSERT INTO cbtb_fatura (";
+					$sSQL .= "	id_cliente_produto, vencimento, cobranca, tx_juros, valor, qtde_faturas ",
+					$sSQL .= ") VALUES (";
+					$sSQL .= "	$id_cliente_produto, $vencimento, $cobranca, "*/
+					
+//FIM DEDÉ
 					
 					
 					
