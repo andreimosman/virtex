df18781a19a49dd63eb4a0be3d85bea3a0be6233
hugo <hugo>
2006-04-11 14:30:23 +0000

    no message

---
+++ doc/EXTRAS.sql
@@ -0,0 +1,5 @@
+INSERT INTO cftb_forma_pagamento (id_cobranca, nome_cobranca, disponivel) values (nextval('cbsq_id_cobranca'),'Boleto Bancário','t');
+INSERT INTO cftb_forma_pagamento (id_cobranca, nome_cobranca, disponivel) values (nextval('cbsq_id_cobranca'),'Carnê','t');
+INSERT INTO cftb_forma_pagamento (id_cobranca, nome_cobranca, disponivel) values (nextval('cbsq_id_cobranca'),'Depósito Bancário','t');
+INSERT INTO cftb_forma_pagamento (id_cobranca, nome_cobranca, disponivel) values (nextval('cbsq_id_cobranca'),'Cartão de Crédito','t');
+INSERT INTO cftb_forma_pagamento (id_cobranca, nome_cobranca, disponivel) values (nextval('cbsq_id_cobranca'),'Fatura Avulsa','t');
