d78efdae3e671c77679799ab85cd09e82dd2edda
hugo <hugo>
2006-05-22 11:08:34 +0000

    no message

--- doc/virtex.sql
+++ doc/virtex.sql
@@ -1,6 +1,6 @@
 /*
 Created		22/2/2006
-Modified		9/4/2006
+Modified		19/5/2006
 Project		
 Model		
 Company		
@@ -96,6 +96,7 @@ Create table  prtb_produto
 	valor_comodato Numeric(7,2) NULL ,
 	desconto_promo Numeric(7,2) NULL ,
 	periodo_desconto Smallint NULL ,
+	tx_instalacao Numeric(7,2) NULL ,
  primary key (id_produto)
 );
 
@@ -115,6 +116,7 @@ Create table  prtb_produto_bandalarga
 	banda_download_kbps Smallint NULL ,
 	franquia_trafego_mensal_gb Smallint NULL ,
 	valor_trafego_adicional_gb Numeric(7,2) NULL ,
+	roteado Boolean NULL ,
  primary key (id_produto)
 );
 
@@ -150,6 +152,7 @@ Create table  cbtb_contrato
 	valor_produto Numeric(7,2) NULL ,
 	num_emails Smallint NULL ,
 	quota_por_conta Smallint NULL ,
+	tx_instalacao Numeric(7,2) NULL ,
 	comodato Boolean NULL ,
 	valor_comodato Numeric(7,2) NULL ,
 	desconto_promo Numeric(7,2) NULL ,
@@ -164,6 +167,20 @@ Create table  cbtb_contrato
 	bl_banda_download_kbps Smallint NULL ,
 	bl_franquia_trafego_mensal_gb Smallint NULL ,
 	bl_valor_trafego_adicional_gb Numeric(7,2) NULL ,
+	cod_banco Smallint NULL ,
+	carteira Smallint NULL ,
+	agencia Smallint NULL ,
+	num_conta Smallint NULL ,
+	convenio Smallint NULL ,
+	cc_vencimento Varchar(5) NULL ,
+	cc_numero Varchar(25) NULL ,
+	cc_operadora Char(2) NULL ,
+	db_banco Smallint NULL ,
+	db_agencia Smallint NULL ,
+	db_conta Smallint NULL ,
+	vencimento Smallint NULL ,
+	carencia Smallint NULL ,
+	data_alt_status Date NULL ,
  primary key (id_cliente_produto)
 );
 
@@ -258,6 +275,7 @@ Create table  cntb_conta_bandalarga
 	status Char(1) NULL ,
 	mac Macaddr NULL ,
 	id_nas Smallint NOT NULL,
+	ip_externo Inet NOT NULL,
  primary key (username,tipo_conta,dominio)
 );
 
@@ -266,7 +284,7 @@ Create table  cntb_conta_discado
 	username Varchar(30) NOT NULL,
 	tipo_conta Varchar(2) NOT NULL,
 	dominio Varchar(255) NOT NULL,
-	foneinfo Varchar(15) NULL ,
+	foneinfo Varchar(64) NULL ,
  primary key (username,tipo_conta,dominio)
 );
 
@@ -342,6 +360,15 @@ Create table  cftb_preferencias
 	tx_juros Smallint NULL ,
 	multa Smallint NULL ,
 	dia_venc Smallint NULL ,
+	carencia Smallint NULL ,
+	cnpj Char(25) NULL ,
+	cod_banco Smallint NULL ,
+	agencia Smallint NULL ,
+	num_conta Smallint NULL ,
+	observacoes Text NULL ,
+	carteira Smallint NULL ,
+	convenio Smallint NULL ,
+	pagamento Char(3) NULL ,
  primary key (id_provedor)
 );
 
@@ -359,13 +386,166 @@ Create table  lgtb_exclusao
 Create table  cbtb_faturas
 (
 	id_cliente_produto Smallint NOT NULL,
-	vencimento Date NOT NULL,
-	tipo_cobranca Smallint NULL ,
-	tx_juros Smallint NULL ,
-	valor Numeric(7,2) NULL ,
-	qtde_faturas Smallint NULL ,
- primary key (id_cliente_produto,vencimento)
+	data Date NOT NULL,
+	descricao Text NULL ,
+	valor Numeric(7,2) NOT NULL,
+	status Varchar(2) NULL ,
+	observacoes Text NULL ,
+	reagendamento Date NULL ,
+	pagto_parcial Numeric(7,2) NULL ,
+	data_pagamento Date NULL ,
+	desconto Numeric(7,2) NULL ,
+	acrescimo Numeric(7,2) NULL ,
+	valor_pago Numeric(7,2) NULL ,
+	id_cobranca Serial NULL ,
+	cod_barra Varchar(50) NULL  UNIQUE ,
+	anterior Boolean NULL  Default false,
+	id_carne Smallint NOT NULL,
+ primary key (id_cliente_produto,data)
 );
+
+Create table  lgtb_bloqueio_automatizado
+(
+	id_processo Smallint NOT NULL,
+	id_cliente_produto Smallint NOT NULL,
+	data_hora Timestamp NOT NULL Default now(),
+	tipo Varchar(1) NOT NULL,
+	admin Varchar(20) NULL ,
+	auto_obs Varchar(50) NULL ,
+	admin_obs Text NULL ,
+ primary key (id_processo)
+);
+
+Create table  pftb_preferencia_cobranca
+(
+	id_provedor Smallint NOT NULL UNIQUE ,
+	tx_juros Smallint NULL ,
+	multa Smallint NULL ,
+	dia_venc Smallint NULL ,
+	carencia Smallint NULL ,
+	cod_banco Smallint NULL ,
+	carteira Smallint NULL ,
+	agencia Smallint NULL ,
+	num_conta Smallint NULL ,
+	convenio Smallint NULL ,
+	observacoes Text NULL ,
+	pagamento Char(3) NULL ,
+	path_contrato Varchar(255) NULL ,
+ primary key (id_provedor)
+);
+
+Create table  pftb_preferencia_geral
+(
+	id_provedor Smallint NOT NULL UNIQUE ,
+	dominio_padrao Varchar(150) NULL ,
+	nome Varchar(255) NULL ,
+	radius_server Inet NULL ,
+	hosp_server Inet NULL ,
+	hosp_ns1 Inet NULL ,
+	hosp_ns2 Inet NULL ,
+	hosp_uid Smallint NULL ,
+	hosp_gid Smallint NULL ,
+	mail_server Inet NULL ,
+	mail_uid Smallint NULL ,
+	mail_gid Smallint NULL ,
+	pop_host Varchar(255) NULL ,
+	smtp_host Varchar(255) NULL ,
+	hosp_base Varchar(255) NULL ,
+ primary key (id_provedor)
+);
+
+Create table  pftb_preferencia_provedor
+(
+	id_provedor Smallint NOT NULL UNIQUE ,
+	endereco Varchar(255) NULL ,
+	localidade Varchar(255) NULL ,
+	cep Varchar(20) NULL ,
+	cnpj Char(25) NULL ,
+ primary key (id_provedor)
+);
+
+Create table  lgtb_backup
+(
+	id_backup Smallint NOT NULL,
+	admin Varchar(50) NULL ,
+	data Date NULL ,
+	nome_arq Varchar(100) NULL ,
+	status Char(2) NULL ,
+	bd_dados Boolean NULL ,
+	bd_struct Boolean NULL ,
+	cfg_so Boolean NULL ,
+	cfg_vtx Boolean NULL ,
+	cfg_utilitarios Boolean NULL ,
+	obs Text NULL ,
+ primary key (id_backup)
+);
+
+Create table  rdtb_accounting
+(
+	session_id Varchar(64) NOT NULL,
+	username Varchar(64) NULL ,
+	login Timestamp NULL  Default now(),
+	logout Timestamp NULL ,
+	tempo Numeric(30,0) NULL ,
+	caller_id Varchar(30) NULL ,
+	nas Varchar(128) NULL ,
+	framed_ip_address Varchar(20) NULL ,
+	terminate_cause Varchar(128) NULL ,
+	bytes_in Numeric(30,0) NULL ,
+	bytes_out Numeric(30,0) NULL ,
+ primary key (session_id)
+);
+
+Create table  rdtb_log
+(
+	id_log Smallint NOT NULL Default nextval('rdtb_log_id_log'),
+	datahora Timestamp NULL  Default now(),
+	tipo Varchar(2) NULL ,
+	username Varchar(64) NULL ,
+	mensagem Text NULL ,
+	caller_id Varchar(30) NULL ,
+ primary key (id_log)
+);
+
+Create table  cbtb_carne
+(
+	id_carne Smallint NOT NULL,
+	data_geracao Smallint NULL ,
+	status Varchar(2) NULL  Default 'A',
+	id_cliente_produto Smallint NULL ,
+	valor Numeric(30,2) NULL ,
+	vigencia Smallint NULL ,
+	id_cliente Smallint NULL ,
+ primary key (id_carne)
+);
+
+Create table  lgtb_reagendamento
+(
+	id_reagendamento Serial NOT NULL,
+	id_cliente_produto Smallint NOT NULL,
+	data Date NOT NULL,
+	admin Smallint NULL ,
+	data_reagendamento Date NULL  Default now(),
+	data_para_reagendamento Date NULL ,
+ primary key (id_reagendamento)
+);
+
+Create table  cftb_ip_externo
+(
+	ip_externo Inet NOT NULL,
+	id_nas Smallint NOT NULL,
+ primary key (ip_externo)
+);
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -410,6 +590,7 @@ Alter table prtb_produto_hospedagem add  foreign key (id_produto) references prt
 Alter table cbtb_cliente_produto add  foreign key (id_produto) references prtb_produto (id_produto)  on update restrict  on delete restrict ;
 Alter table cbtb_contrato add  foreign key (id_cliente_produto) references cbtb_cliente_produto (id_cliente_produto)  on update restrict  on delete restrict ;
 Alter table cntb_conta add  foreign key (id_cliente_produto) references cbtb_cliente_produto (id_cliente_produto)  on update restrict  on delete restrict ;
+Alter table lgtb_bloqueio_automatizado add  foreign key (id_cliente_produto) references cbtb_cliente_produto (id_cliente_produto)  on update restrict  on delete restrict ;
 Alter table cbtb_faturas add  foreign key (id_cliente_produto) references cbtb_contrato (id_cliente_produto)  on update restrict  on delete restrict ;
 Alter table cntb_conta add  foreign key (dominio) references dominio (dominio)  on update restrict  on delete restrict ;
 Alter table cbtb_contrato add  foreign key (id_cobranca) references cftb_forma_pagamento (id_cobranca)  on update restrict  on delete restrict ;
@@ -426,6 +607,10 @@ Alter table cntb_conta_bandalarga add  foreign key (rede) references cftb_rede (
 Alter table cftb_nas_rede add  foreign key (rede) references cftb_rede (rede)  on update restrict  on delete restrict ;
 Alter table cftb_nas_rede add  foreign key (id_nas) references cftb_nas (id_nas)  on update restrict  on delete restrict ;
 Alter table cntb_conta_bandalarga add  foreign key (id_nas) references cftb_nas (id_nas)  on update restrict  on delete restrict ;
+Alter table cftb_ip_externo add  foreign key (id_nas) references cftb_nas (id_nas)  on update restrict  on delete restrict ;
+Alter table lgtb_reagendamento add  foreign key (id_cliente_produto,data) references cbtb_faturas (id_cliente_produto,data)  on update restrict  on delete restrict ;
+Alter table cbtb_faturas add  foreign key (id_carne) references cbtb_carne (id_carne)  on update restrict  on delete restrict ;
+Alter table cntb_conta_bandalarga add  foreign key (ip_externo) references cftb_ip_externo (ip_externo)  on update restrict  on delete restrict ;
 
 
 CREATE SEQUENCE adsq_id_admin;
@@ -441,5 +626,10 @@ CREATE SEQUENCE clsq_id_conta;
 CREATE SEQUENCE prsq_id_produto;
 CREATE SEQUENCE sptb_spool_id_spool;
 CREATE SEQUENCE lgsq_id_exclusao;
+CREATE SEQUENCE lgtb_backup_id_backup;
+CREATE SEQUENCE rdtb_accounting_session_id;
+CREATE SEQUENCE rdtb_log_id_log;
+CREATE SEQUENCE rdsq_id_accounting;
+CREATE SEQUENCE cbsq_id_carne;
 
 
