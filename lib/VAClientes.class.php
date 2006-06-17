7e8403ad5d98ca1a374410eee9d8d70010b0ce6f
hugo <hugo>
2006-06-17 22:41:23 +0000

    no message

--- lib/VAClientes.class.php
+++ lib/VAClientes.class.php
@@ -823,7 +823,34 @@ class VAClientes extends VirtexAdmin {
 
 		$this->arquivoTemplate = "cliente_cobranca_resumo_faturas.html";
 		return;
-
+	}else if ($tipo_lista == 'contratos'){
+	
+			$sSQL = "SELECT nome_razao FROM cltb_cliente WHERE id_cliente = '$id_cliente'";
+			$cliente = $this->bd->obtemUnicoRegistro($sSQL);
+			
+			
+			$sSQL  = "SELECT ";
+			$sSQL .= "	ct.id_cliente_produto, ct.data_contratacao, ct.vigencia, ct.id_produto, ct.tipo_produto, ct.valor_contrato, ct.status, ";
+			$sSQL .= "	cl.id_cliente_produto, cl.id_cliente, ";
+			$sSQL .= "	pr.id_produto, pr.nome ";
+			$sSQL .= "FROM ";
+			$sSQL .= "	cbtb_contrato ct, cbtb_cliente_produto cl, prtb_produto pr ";
+			$sSQL .= "WHERE ";
+			$sSQL .= "	cl.id_cliente_produto = ct.id_cliente_produto  AND cl.id_cliente = '$id_cliente' AND ct.id_produto = pr.id_produto ";
+			$sSQL .= " ORDER BY ct.data_contratacao DESC ";
+			
+			$lista_contrato = $this->bd->obtemRegistros($sSQL);
+			
+			//////echo "lista: $sSQL <br>";
+				$this->tpl->atribui("lista_contrato",$lista_contrato);
+				$this->tpl->atribui("cliente",$cliente);
+				$this->tpl->atribui("id_cliente", $id_cliente);
+			//$this->tpl->atribui("lista_faturas",$lista_faturas);
+			$this->arquivoTemplate = "cliente_contratos_todos.html";
+			return;
+	
+	
+	
 	}else{
 		
 		$sSQL  = "SELECT ";
