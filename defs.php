7e9444b5e21b95e6e090d4482bd25e219823822c
hugo <hugo>
2006-08-03 12:23:41 +0000

    no message

--- defs.php
+++ defs.php
@@ -39,9 +39,9 @@ require_once(PATH_LIB . '/VAInterface_cliente_home.class.php');
 require_once(PATH_LIB . '/spool_hospedagem.class.php');
 
 
-function __autoload($class_name) {
-   require_once PATH_LIB . "/" . $class_name . '.class.php';
-}
+//function __autoload($class_name) {
+//   require_once PATH_LIB . "/" . $class_name . '.class.php';
+//}
 
 session_start();
 
