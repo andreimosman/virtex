27970b95906861b89ef9a9b19224a14abdf90b47
hugo <hugo>
2006-08-24 18:28:27 +0000

    no message

--- lib/VABackup.class.php
+++ lib/VABackup.class.php
@@ -185,7 +185,7 @@ class VABackup extends VirtexAdmin {
 					$id_backup = @$_REQUEST["id_backup"];
 					
 					$sSQL = "SELECT * FROM bxtb_backup where id_backup = $id_backup";
-					$bkp = $this->obtemUnicoRegistro($sSQL);
+					$bkp = $this->bd->obtemUnicoRegistro($sSQL);
 					
 					$arquivo = "/mosman/backup/".$bkp["arquivo_backup"];
 					$arq_down = fopen($arquivo,"r");
