1204dfe84331d6e2cb49738e9a49ffd30a42acb8
hugo <hugo>
2006-08-24 17:19:10 +0000

    no message

---
+++ lib/VABackup.class.php
@@ -0,0 +1,235 @@
+<?
+require_once( PATH_LIB . "/VirtexAdmin.class.php" );
+
+
+class VABackup extends VirtexAdmin {
+
+	public function VABackup() {
+		parent::VirtexAdmin();
+	}
+
+	public function processa($op=null) {	
+		if( ! $this->privPodeGravar("_SUPORTE_BACKUP") ) {
+			$this->privMSG();
+			return;
+		}	
+
+	
+		if ($op == "inicio"){
+		
+			$sSQL = "SELECT b.id_backup,b.data_backup,b.arquivo_backup,b.tipo_backup,b.status_backup,b.admin,a.admin FROM bktb_backup b,adtb_admin a WHERE b.id_admin = a.id_admin ORDER BY id_backup,arquivo_backup DESC LIMIT 10";
+			$lista = $this->bd->obtemRegistros($sSQL);
+			
+			$this->tpl->atribui("lista_backups",$lista);
+			$this->arquivoTemplate = "backup_inicio.html";
+			
+		
+		
+		
+		}else if ($op == "backup"){
+		
+			$configuracao = @$_REQUEST["configuracao"];
+			$bd = @$_REQUEST["bd"];
+			$sistema = @$_REQUEST["sistema"];
+			$hoje = DATE("d-m-Y-H-i-s");
+			//ECHO $hoje ."<br>";
+			$acao = @$_REQUEST["acao"];
+			$op = @$_REQUEST["op"];
+			$sop = @$_REQUEST["sop"];
+			$msg = "";
+			$admin = $this->admLogin->obtemId();
+
+
+			if ($acao == "backup") {
+			
+				$operacao = 
+				//echo "acao <br>";
+				$erro = 0;
+			
+				if ($sop == "ok"){
+				
+				//echo "ok<br>";
+					if($bd){
+					//echo "banco<br>";
+						
+						$arquivo = "bd_$hoje.sql";
+						system('pg_dump --disable-triggers -U virtex > /mosman/backup/'.$arquivo, $retvalbd);
+						
+						if ($retvalbd != 0){
+						
+							$status = "ERRO";
+								
+						}else{
+						
+							$status = "OK";
+							
+						}
+						
+						$sSQL  = "INSERT INTO bktb_backup ";
+						$sSQL .= "(data_backup,arquivo_backup,tipo_backup,status_backup,admin,operador_backup) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "('$hoje','$arquivo','Banco de Dados','$status','$admin','GU') ";
+						$this->bd->consulta($sSQL);
+						
+						//$msg .= $retvalbd."<br>";
+
+
+					}
+					if($configuracao){
+					
+						$pathbackup = " /mosman/backup/";
+					
+						$nome1 = "etc_$hoje.tgz";
+						$nome2 = "appetc_$hoje.tgz";
+						
+						$caminho1 = " /mosman/virtex/etc/";
+						$caminho2 = " /mosman/virtex/app/etc/";
+						
+						$comando1 = "tar -czvf $pathbackup$nome1  $caminho1";
+						$comando2 = "tar -czvf $pathbackup$nome2  $caminho2";
+						
+					
+						system($comando1,$retvalconf1);
+						
+						
+						if ($retvalconf1 !=0){
+						
+							$status = "ERRO";
+						
+						}else{
+						
+							$status = "OK";
+						
+						}
+						
+						
+						$sSQL  = "INSERT INTO bktb_backup ";
+						$sSQL .= "(data_backup,arquivo_backup,tipo_backup,status_backup,admin,operador_backup) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "('$hoje','$nome1','Configurações','$status','$admin','GU') ";
+						$this->bd->consulta($sSQL);
+
+						system($comando2,$retvalconf2);
+						
+						
+						
+						if ($retvalconf2 !=0){
+						
+							$status = "ERRO";
+						
+						}else{
+						
+							$status = "OK";
+						
+						}
+						
+						$sSQL  = "INSERT INTO bktb_backup ";
+						$sSQL .= "(data_backup,arquivo_backup,tipo_backup,status_backup,admin,operador_backup) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "('$hoje','$nome2','Configurações','$status','$admin','GU') ";
+						$this->bd->consulta($sSQL);		
+						
+						
+						
+					}
+					if($sistema){
+					
+						$pathbackup = " /mosman/backup/";
+										
+						$nome = "virtex_$hoje.tgz";
+						$caminho = " /mosman/virtex/";
+					
+						$comando = "tar -czvf $pathbackup$nome  $caminho";
+					
+						system($comando,$retvalsystem);
+						
+						//echo "comando: $comando<br>";
+						
+						//copy("/mosman/virtex/app/virtex_".$hoje.".tgz","/mosman/backup/sys/virtex_".$hoje.".tgz");
+						//$msg .= $retvalsystem."<br>";
+						if ($retvalsystem != 0){
+						
+							$status = "ERRO";
+						
+						}else{
+						
+							$status = "OK";
+						
+						}
+						
+						$sSQL  = "INSERT INTO bktb_backup ";
+						$sSQL .= "(data_backup,arquivo_backup,tipo_backup,status_backup,admin,operador_backup) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "('$hoje','$nome','Sistema','$status','$admin','GU') ";
+						$this->bd->consulta($sSQL);		
+						
+						
+						
+						
+					}
+					
+					
+					$sSQL = "SELECT * FROM bktb_backup WHERE data_backup = '$hoje' ORDER BY id_backup,arquivo_backup ";
+					$lista = $this->bd->obtemRegistros($sSQL);
+					
+					$this->tpl->atribui("lista",$lista);
+					$this->arquivoTemplate = "backup_final.html";
+					return;
+			
+				}else if ($sop == "download"){
+				
+					$id_backup = @$_REQUEST["id_backup"];
+					
+					$sSQL = "SELECT * FROM bxtb_backup where id_backup = $id_backup";
+					$bkp = $this->obtemUnicoRegistro($sSQL);
+					
+					$arquivo = "/mosman/backup/".$bkp["arquivo_backup"];
+					$arq_down = fopen($arquivo,"r");
+					
+					header('Pragma: public');
+					header('Expires: 0');
+					header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+					header("Content-type: application/octet-stream ");
+					header('Content-Disposition: attachment; filename="$arq_down"');
+					readfile($arq_down);
+				
+					fclose($arq_down);
+				
+				}
+
+			}
+		
+		
+			$this->arquivoTemplate = "backup_efetua.html";
+		
+		
+		
+		
+		
+		
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}// final de processa
+
+}// final da classe
+?>
