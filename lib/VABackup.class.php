4bef751ec69a3c6c63bc3c04ffcede7b6e7bb7a7
hugo <hugo>
2006-08-26 17:47:48 +0000

    no message

--- lib/VABackup.class.php
+++ lib/VABackup.class.php
@@ -302,8 +302,18 @@ class VABackup extends VirtexAdmin {
 			
 			$acao = @$_REQUEST["acao"];
 			
+			$hoje = DATE("Y-m-d");
+			$DATA = date("Y-m-d H:i:s");
+
+
+			$DATA2 = str_replace(" ","_",$DATA);
+			$DATA2 = str_replace(":","-",$DATA2);
 			
-			$sSQL = "SELECT b.data, b.id_backup from bktb_backup b ORDER BY b.data DESC";
+			$msg = "";
+			$admin = $this->admLogin->obtemId();			
+			
+			
+			$sSQL = "SELECT b.data, b.id_backup from bktb_backup b, bktb_arquivo a WHERE b.id_backup = a.id_backup AND a.tipo_backup = 'Banco de Dados' ORDER BY b.data DESC";
 			$data = $this->bd->obtemRegistros($sSQL);
 			//echo "datas: $sSQL<br>";
 			
@@ -338,7 +348,71 @@ class VABackup extends VirtexAdmin {
 					$this->arquivoTemplate = "restore_confirma.html";
 					return;
 
+					if ($acao == "ok"){
 					
+						$arq = $_REQUEST["arquivo"];
+						
+						
+						$arquivo = "bd_$DATA2.gz";
+						system('pg_dump --clean --disable-triggers --compress=9 -U virtex > /mosman/backup/'.$arquivo, $retvalbd);
+												
+						if ($retvalbd != 0){
+												
+							$status = "ERRO";
+							$erro = 1;
+														
+						}else{
+												
+							$status = "OK";
+													
+						}
+												
+						$sSQL  = "INSERT INTO bktb_backup ";
+						$sSQL .= "(data_backup,admin,operador_backup,data) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "('$hoje','$admin','GS','$DATA') ";
+						$this->bd->consulta($sSQL);
+
+												
+						$sSQL  = "INSERT INTO bktb_arquivos ";
+						$sSQL .= "(id_backup,arquivo_backup, tipo_backup, status_backup, data_backup) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "((select max(id_backup) FROM bktb_backup),'$arquivo', 'Banco de Dados','$status', '$hoje' )";
+						$this->bd->consulta($sSQL);
+						//ECHO "GRAVAÇÃO DE BKP´: $sSQL<br>";
+						
+						
+						
+						
+						//FAZ O RESTORE
+						
+						$comando = "zcat /mosman/backup/$arq |psql -U virtex ";
+					
+						system($comando,$retval);
+						
+						if ($retval == 1){
+						
+							$msg = "ERRO";
+						
+							
+						
+						}else{
+							
+							$msg = "OK";
+						
+						}
+					
+						$sSQL = "INSERT INTO lgtb_restore (arquivo_restore, data_restore, admin, status_restore) ";
+						$sSQL .= "VALUES ";
+						$sSQL .= "('$arq','$DATA', '$admin', '$msg') ";
+						$this->bd->consulta($sSQL);
+						
+						$this->tpl->atribui("mensagem",$msg);
+						$this->ArquivoTemplate = "restore_final.html";
+						return;
+
+					
+					}
 
 
 
