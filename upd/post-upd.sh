08863367bff7a2fc8f4f51db34fc833f7521cf50
mosman <mosman>
2006-11-29 10:43:02 +0000

    no message

---
+++ upd/post-upd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+############################################
+# Script de pos-atualizacao do VirtexAdmin #
+#                                          #
+# Objetivo: Ajustar permissões de arquivos #
+#           e pastas                       #
+###########################################
+
+
+chmod +x /mosman/virtex/app/bin/rc
+chmod +x /mosman/virtex/app/cgi/stats
+
+
