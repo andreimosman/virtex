30fa9b9752644dec6f02ef5b5ab4f68cca37c984
hugo <hugo>
2006-12-22 16:52:47 +0000

    no message

--- lib/VAPapocker.class.php
+++ lib/VAPapocker.class.php
@@ -1,6 +1,6 @@
 <?
 
-class VAPapocker extends VirtexAdminWeb {
+class VAPapocker extends VirtexAdmin {
 
 	public function VAPapocker() {
 		parent::VirtexAdmin();
@@ -73,6 +73,81 @@ class VAPapocker extends VirtexAdminWeb {
 			
 			
 			
+		
+		
+		}else if ($op == "natal"){
+		
+		
+			$sSQL = "SELECT username, dominio, tipo_conta FROM cntb_conta WHERE tipo_conta = 'E' and status = 'A'";
+			//$sSQL .= " AND username = 'web' AND dominio = 'mosman.com.br' ";
+			$emails = $this->bd->obtemRegistros($sSQL);
+			
+			for ($i=0;$i<count($emails);$i++){
+			
+				$email = $emails[$i]["username"]."@".$emails[$i]["dominio"];
+				
+				//echo $i ."-". $email ."<br>";
+				
+				// MANDA O EMAIL COM ANEXO
+				
+				$from = "firme@firme.com.br";
+				$subject = "FELIZ NATAL A TODOS";
+								
+				$mailheaders = "From: $from\n";
+				$mailheaders .= "Reply-To: $from\n";
+				//$mailheaders .= "Cc: $cc\n";
+				//$mailheaders .= "Bcc: $bcc\n";
+				$mailheaders .= "X-Mailer: Script para enviar arquivo atachado\n";
+				
+				$msg_body = stripslashes($body); 
+				
+				
+				
+				$attach = "/mosman/virtex/dados/hospedagem/mosman.com.br/lixo/Feliz_Natal.jpg";
+				
+				$file = fopen($attach, "r");
+				//$attach_size = $file['size'];
+				$contents = fread($file, filesize($attach));
+				$encoded_attach = chunk_split(base64_encode($contents));
+				fclose($file);
+				
+				$attach_name = "Feliz_Natal.jpg";
+				
+				
+				$mailheaders .= "MIME-version: 1.0\n";
+				$mailheaders .= "Content-type: multipart/mixed; ";
+				$mailheaders .= "boundary=\"Message-Boundary\"\n";
+				$mailheaders .= "Content-transfer-encoding: 7BIT\n";
+				$mailheaders .= "X-attachments: $attach_name";
+
+
+				$body_top = "--Message-Boundary\n";
+				$body_top .= "Content-type: text/plain; charset=US-ASCII\n";
+				$body_top .= "Content-transfer-encoding: 7BIT\n";
+				$body_top .= "Content-description: Mail message body\n\n";
+				
+				$msg_body = $body_top . $msg_body;
+				
+				$msg_body .= "\n\n--Message-Boundary\n";
+				$msg_body .= "Content-type: image/jpeg; name=\"$attach_name\"\n";
+				$msg_body .= "Content-Transfer-Encoding: BASE64\n";
+				$msg_body .= "Content-disposition: inline; filename=\"$attach_name\"\n\n";
+				
+				$msg_body .= "$encoded_attach\n";
+				$msg_body .= "--Message-Boundary--\n";
+				$msg_body .="
+									A FIRME.COM.BR E A VIRTEX.COM.BR DESEJAM A TODOS UM FELIZ NATAL
+									
+									
+				";
+				
+				mail($email,$subject,$msg_body,$mailheaders);
+				//mail($to, stripslashes($subject), $msg_body, $mailheaders);  
+				
+				echo $i ."-". $email ."<br>";
+			
+			}
+		
 		
 		
 		}
